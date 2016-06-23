ecb508c8691e3db59e8794384c39ab45e66677f4
Ronaldo Felipe <ronaldolocalhost@gmail.com>
2016-06-22 22:36:10 -0300

    Cardboard Experiment

---
+++ CardboardExperiment/Assets/Core/Scripts/RotateAround.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class RotateAround : MonoBehaviour, IGvrGazeResponder
+{
+
+    public float _speed = 5.0f;
+
+    public void Rotate() {
+        transform.Rotate(Vector3.up*_speed);
+    }
+
+    public void OnGazeEnter()
+    {
+        InvokeRepeating("OnGazeStay", 0.0f, 1.0f / 30.0f);
+    }
+
+    public void OnGazeExit()
+    {
+        CancelInvoke("OnGazeStay");
+    }
+
+    public void OnGazeStay() {
+        Rotate();
+    }
+    
+    public void OnGazeTrigger()
+    {
+        throw new NotImplementedException();
+    }
+}
